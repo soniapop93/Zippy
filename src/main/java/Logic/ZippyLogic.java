5cd87117a765d94004c5b05ee13f59cb95ded4a1
soniapop93 <popsonia85@gmail.com>
2023-11-11 23:49:48 +0200

    implemented 2 small functions in the zippy logic

--- src/main/java/Logic/ZippyLogic.java
+++ src/main/java/Logic/ZippyLogic.java
@@ -4,10 +4,32 @@ import java.util.Scanner;
 
 public class ZippyLogic {
     Scanner scanner = new Scanner(System.in);
+    String[] options = new String[] {"1. Zip file", "2. Unzip file", "3. Zip Directory", "4. Unzip Directory", "5. Exit"};
 
+    public void zippy() {
+        welcomeMessage();
+
+        while (true) {
+            showOptions();
+            
+        }
+
+    }
+
+    private void welcomeMessage() {
         System.out.println("-------------------");
         System.out.println(" Welcome to Zippy");
         System.out.println("-------------------");
+    }
 
-        System.out.println("Selection options: ");
+    private void showOptions() {
+        System.out.println("Select option: ");
+
+        for (String item : options) {
+            System.out.println("--> " + item);
+        }
+    }
+
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import Logic.ZippyLogic;
+
 public class Main {
     public static void main(String[] args) {
-
+        ZippyLogic zippyLogic = new ZippyLogic();
+        zippyLogic.zippy();
 
         }
 }
