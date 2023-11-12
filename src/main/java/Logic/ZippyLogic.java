3438ef572e47428e49ad92c93ec963134081455a
soniapop93 <popsonia85@gmail.com>
2023-11-12 22:42:32 +0200

    started to implement logic

--- src/main/java/Logic/ZippyLogic.java
+++ src/main/java/Logic/ZippyLogic.java
@@ -7,11 +7,37 @@ public class ZippyLogic {
     String[] options = new String[] {"1. Zip file", "2. Unzip file", "3. Zip Directory", "4. Unzip Directory", "5. Exit"};
 
     public void zippy() {
+        String inputOption;
+
         welcomeMessage();
 
         while (true) {
             showOptions();
-            
+
+            inputOption = scanner.nextLine();
+
+            switch (inputOption) {
+                default:
+                    System.out.println(">> Option not recognized...");
+                    break;
+                case "1": // 1. Zip file
+                    System.out.println(">> Option selected: " + options[0]);
+                    break;
+                case "2": // 2. Unzip file
+                    System.out.println(">> Option selected: " + options[1]);
+                    break;
+                case "3": // 3. Zip Directory
+                    System.out.println(">> Option selected: " + options[2]);
+                    break;
+                case "4": // 4. Unzip Directory
+                    System.out.println(">> Option selected: " + options[3]);
+                    break;
+                case "5": // 5. Exit
+                    System.out.println(">> Option selected: " + options[4]);
+                    return;
+            }
+
+
         }
 
     }
